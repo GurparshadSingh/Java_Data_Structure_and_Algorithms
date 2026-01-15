01b07b1f405c4508577837292cccc194d92af0a2
Gurparshad Singh <gurparshad0878@gmail.com>
2026-01-15 19:09:07 +0530

    Add files via upload

---
+++ constructors.java
@@ -0,0 +1,18 @@
+public class constructors {
+    public static void main(String[] args) {
+        Student s1 =  new Student("GPS");
+        System.out.println(s1.name);
+        
+    }
+
+    static class Student{
+        String name;
+        // int roll;
+
+        Student(String name) {
+         this.name = name;
+        }
+
+        
+    }
+}
